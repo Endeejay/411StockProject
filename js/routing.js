bfb4f8f7fabfbe291253be096aec81b427225c9e
w0493574 <jenna.mchugh@selu.edu>
2016-10-22 19:10:31 -0500

    highcharts working again, call to makeCharts.js was missing

--- js/routing.js
+++ js/routing.js
@@ -1,5 +1,5 @@
 // app.js
-var stockApp = angular.module('stockApp', ['ui.router'/*,'app.directives.makeCharts'*/]);
+var stockApp = angular.module('stockApp', ['ui.router','app.directives.makeCharts']);
 
 stockApp.run(['$rootScope', '$state',
   function($rootScope, $state, $stateParams) {
