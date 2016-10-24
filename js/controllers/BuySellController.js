0d721afb196c4d16fa7d27dddb5fcebbd600c943
Derek Boudreaux <Derek Boudreaux>
2016-10-24 13:17:31 -0500

    Fixed the data being put into the charts, along with the date format being correctly made. As well as slightly altering the look of the charts

--- js/controllers/BuySellController.js
+++ js/controllers/BuySellController.js
@@ -25,33 +25,75 @@ getName();
 function getName(){
  BuySellService.getAllStocks().then(function(data){
    data = data.data;
-   debugger;
    makeChart(data)
   }, function(error){
     console.log(error);
   });
 }
 
-/*function getJsonObject(data) {
-  debugger;
-    $scope.daStock = data.data;
-}*/
-
 function makeChart(data){
-  debugger;
   angular.forEach(data, function(value, key) {
+    //changing our dates to utc dates
+    var UTCDates = [];
+    for(i=0;i<value.Dates.length;i++){
+      var parts = value.Dates[i].split('/');
+      var utcDate = Date.UTC(parts[2],parts[0]-1,parts[1]);
+      UTCDates.push(utcDate);
+    }
+    console.log(UTCDates);
+    //getting our dat array, and then bigArray and littleArray
+    var dat = $.map(UTCDates, function(v,i) {return [v,value.Prices[i]]; });
+    var bigArray=[];
+    var littleArray = [], size = 2;
+    //pusing our spliced array shit to the bigArray
+    while (dat.length > 0) {
+      bigArray.push(dat.splice(0,size));
+    }
+
     $scope.item = {
+        colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066', '#eeaaee',
+          '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
         chart: {
             renderTo: 'container',
+            backgroundColor: '#3e3e40'
         },
         title: {
-            text: value.Name
+            text: value.Name,
+            style: { 
+              color: 'white'
+            }
+        },
+        subtitle: {
+            text: value.Symbol,
+            style: { 
+              color: '#E0E0E3'
+            }
+        },
+        yAxis: {
+          title: {
+            text: 'Prices',
+            style: { 
+              color: 'white'
+            }
+          }
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%Y-%m-%d}',
+            rotation: 45,
+            align: 'left'
+          },
+          title: {
+              text: 'Dates',
+              style: { 
+              color: 'white'
+            }
+          }
         },
         series: [{
             type: 'line',
-            data: [
-                [value.Dates, value.Prices]
-            ]
+            data: bigArray
         }],
         credits: {
             enabled: false
