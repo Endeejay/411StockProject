400e38204ceee55dcd4d5871fa7c31d8b06dc636
Jenna McHugh <jenna.mchugh@selu.edu>
2016-11-03 18:19:12 -0500

    cleaned up buySellDetails template, added functionality to buttons, fixed bug when refreshing that sent user back to the buySellDetails without any data

--- js/controllers/DetailsController.js
+++ js/controllers/DetailsController.js
@@ -1,4 +1,21 @@
-stockApp.controller('DetailsController', function($scope, $stateParams) {
+stockApp.controller('DetailsController', function DetailsController($scope, $stateParams, $state) {
+    if($stateParams.stockObj == null){
+    	if($state.is('live.buy_sell.details')){
+    		$state.go('live.buy_sell');
+    	}
+    	else{
+    		$state.go('historic.buy_sell');
+    	}
+    }
+
     var stockObj = $stateParams.stockObj;
     console.log(stockObj);
+
+    $scope.buyStock = function(){
+    	console.log("Bought one of " + stockObj.Symbol);
+    }
+
+    $scope.sellStock = function(){
+    	console.log("Sold one of " + stockObj.Symbol);
+    }
 });
