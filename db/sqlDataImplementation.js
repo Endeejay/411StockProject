5cba9d92612d2421f8615fd9b3242902dbe0359b
w0493574 <jenna.mchugh@selu.edu>
2016-11-13 23:30:30 -0600

    possible sqlite implementation. not going into detail since no one seems to read my commit messages, let alone check the branches...

---
+++ db/sqlDataImplementation.js
@@ -0,0 +1,46 @@
+stockApp.service('SQLDBService', [ function(){
+	const sql = require('../node_modules/sql.js/js/sql.js');
+	const fs = require('fs');
+	const path = require('path');
+	
+	//should use electron json storage to save the db in order to have better response time
+	const filebuffer = fs.readFileSync('db/electronTrader.sqlite');
+
+	if (filebuffer != null) {
+		var db = new SQL.Database(filebuffer);
+	} else {
+		var db = new SQL.Database();
+	}
+	/***Function Mappings***/
+	this.initDb = initDb;
+	this.createPortfolio = createPortfolio;
+
+	function initDb(){
+		try {
+			db.run("CREATE TABLE IF NOT EXISTS portfolio (id INT UNIQUE, portfolioName TEXT, isLive INT, startDate TEXT, endDate TEXT, currency TEXT)");
+			db.run("CREATE TABLE IF NOT EXISTS watch (id INT UNIQUE, portfolioId INT, symbol TEXT,FOREIGN KEY(portfolioId) REFERENCES portfolio(id))");
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	function createPortfolio(portfolio){
+		try{
+			var stmt = db.prepare("INSERT INTO portfolio (portfolioName, isLive, startDate, endDate, currency) VALUES(:portfolioName, :isLive, :startDate, :endDate, :currency)");
+			stmt.run([portfolio.portfolioName, portfolio.isLive, portfolio.startDate, portfolio.endDate, portfolio.currency]);
+			saveDb();
+		}catch(e){
+			console.log(e);
+		}
+	}
+	
+	function saveDb(){
+		try {
+			fs.unlinkSync('db/electronTrader.sqlite');
+		} catch (e) {
+			console.log("Unable to delete file; Exception: " + e);
+		}
+		fs.writeFileSync("db/electronTrader.sqlite", new Buffer(db.export()));
+	}
+}]);
+
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,7 @@
-stockApp.controller('MainController', ['$scope', 'DBService', 'APIService', function MainController($scope, DBService, APIService) {
+stockApp.controller('MainController', ['$scope', 'DBService', 'SQLDBService', 'APIService', function MainController($scope, DBService, SQLDBService, APIService) {
 	//APIService.getMultipleStocks(['AAPL', 'GOOGL']);
+	
+	//might take a while to run, could use an animation to show loading feedback
+	SQLDBService.initDb();
 	DBService.initializeDBAtTheBeginningOfStockApp();
 }]);
